4128dc7c6784f8d5a97ddb0ef7efd1482b54681e
Brent Lanier <brent@lanier.us>
2014-07-04 23:08:57 -0400

    Code Update

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ ini_set('display_errors', 1);
 <head>
     <meta charset="utf-8">
     <title>Brent Lanier - P2 - CSCI S-15 (Summer 2014)</title>
-    <!--      Include PHP logic file       -->
-    <?php require 'logic.php' ?>
 
     <link rel='stylesheet' href='style.css' type='text/css'>
 
@@ -17,7 +15,14 @@ ini_set('display_errors', 1);
 
 <body>
 
-    <h1><?php echo getPasswd() ?></h1>
+    <form method="post" action="logic.php">
+
+        Password Length: 0<input type="range" name="numPassWords" min="1" max="10">10
+        <input type="submit" value="Submit" />
+
+    </form>
+
+    <h1><? echo $passwd ?></h1>
 
 </body>
 </html>
