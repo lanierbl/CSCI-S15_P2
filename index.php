f8fb282c061bee7c80c4b6d26ee7c84961b6671e
Brent Lanier <brent@lanier.us>
2014-07-06 22:48:36 -0400

    Finished AJAX & JSON

--- index.php
+++ index.php
@@ -9,22 +9,89 @@ ini_set('display_errors', 1);
     <meta charset="utf-8">
     <title>Brent Lanier - P2 - CSCI S-15 (Summer 2014)</title>
 
+    <!-- Site PHP logic -->
     <?php require 'logic.php' ?>
 
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- jQuery UI -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js"></script>
+    <!-- jQuery UI CSS -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
+    <!-- Site CSS -->
     <link rel='stylesheet' href='style.css' type='text/css'>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#submit_btn").click(function() {
+                //get input field values
+                var numWords       = $('input[name=numWords]').val();
+                var SpecChar       = document.getElementById("SpecChar").checked ? 1 : 0;
+                var capFirstChar   = document.getElementById("capFirstChar").checked ? 1 : 0;
+                var incNum         = document.getElementById("incNum").checked ? 1 : 0;
+
+                var proceed = true;
+
+                if(proceed)
+                {
+                    //data to be sent to server
+                    post_data = {'numWords':numWords, 'SpecChar':SpecChar, 'capFirstChar':capFirstChar, 'incNum':incNum};
+
+                    //Ajax post data to server
+                    $.post('logic.php', post_data, function(response){
+
+                        //load json data from server and output message
+                        if(response.type == 'error')
+                        {
+                            output = '<div class="error">'+response.text+'</div>';
+                        } else {
+                            output = '<div class="success">'+response.text+'</div>';
+                        }
+
+                        $("#result").hide().html(output).slideDown();
+                    }, 'json');
+
+                }
+            });
+
+            //reset previously set border colors and hide all message on .keyup()
+            $("#passwd_form input, #passwd_form textarea").keyup(function() {
+                $("#passwd_form input, #passwd_form textarea").css('border-color','');
+                $("#result").slideUp();
+            });
+
+        });
+    </script>
 
 </head>
 
 <body>
 
-    <form method="post" action="index.php">
+    <fieldset id="passwd_form">
+    <legend>XKCD Password Options</legend>
+    <div id="result"></div>
+        <label for="numWords"><span>Password Length:  </span>
+            <input name="numWords" id="numWords" type="range" min="2" max="8" step="1" value="2"/>
+        </label>
 
-        Password Length: 1<input type="range" name="numPassWords" min="1" max="8" step="1">8
-        <input type="submit" value="Submit" />
+        <label for="SpecChar"><span>Special Character?</span>
+            <input name="SpecChar" id="SpecChar" type="checkbox"/>
+        </label>
 
-    </form>
+        <label for="capFirstChar"><span>Uppercase first character?</span>
+            <input name="capFirstChar" id="capFirstChar" type="checkbox"/>
+        </label>
 
-    <h1><? echo getPasswd() ?></h1>
+        <label for="incNum"><span>Include Number?</span>
+            <input name="incNum" id="incNum" type="checkbox"/>
+        </label>
+
+        <label><span>&nbsp;</span>
+            <button class="submit_btn" id="submit_btn">Submit</button>
+        </label>
+
+    </fieldset>
+
 
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,29 +1,45 @@
 <?php
+if($_POST)
+{
+    //check $_POST vars are set, exit if any missing
+    if(!isset($_POST["numWords"]) || !isset($_POST["capFirstChar"]))
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Input fields are empty!'));
+        die($output);
+    }
 
-function getPasswd() {
+    $numWords       = $_POST["numWords"];
+    $SpecChar       = $_POST["SpecChar"] == 1 ? true : false;
+    $capFirstChar   = $_POST["capFirstChar"] == 1 ? true : false;
+    $incNum         = $_POST["incNum"] == 1 ? true : false;
 
-    // Initial passwprd variable initialized
+    // Initial password variable initialized
     $passwd = "";
     // Open external file containing list of works
     $file = new SplFileObject('wordlist.txt');
     // Set max word list of external file
     $maxFileWords = 58000;
-    // Set number of Passwords in returned string
-    $numPassWords = isset($_POST["numPassWords"]) ? $_POST["numPassWords"] : 0;
+    // String of random characters
+    $randChars = '!@#$%^&*';
     // Loop count = requested password length
-    for($i = 0; $i < $numPassWords; $i++) {
+    for($i = 0; $i < $numWords; $i++) {
         // Determine random int to determine which word to retrieve
         $fileLine = rand(1,$maxFileWords);
         // Open external file and go to specified line
         $file->seek($fileLine-1);
+        // Append special char if desired
+        $passwd = $passwd.($SpecChar ? $randChars[rand(0, strlen($randChars)-1)] : '');
+        // Append number if desired
+        $passwd = $passwd.($incNum ? rand(0,9) : '');
         // Append word to existing passwd string
-        $passwd = $passwd.trim($file->current());
+        $passwd = $passwd.trim($capFirstChar ? ucwords($file->current()) : $file->current());
         // If loop continuing, add a "-" between words
-        if ($i < ($numPassWords - 1)) {
+        if ($i < ($numWords - 1)) {
             $passwd = $passwd."-";
         };
     }
-    return $passwd;
-}
+    $output = json_encode(array('type'=>'message', 'text'=>$passwd));
+    die($output);
 
+}
 
