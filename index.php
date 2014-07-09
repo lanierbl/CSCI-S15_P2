f8dc2ea83bc5c6661aac7d9929c6faf3644d077b
Brent Lanier <brent@lanier.us>
2014-07-08 21:01:21 -0400

    index.php update

--- index.php
+++ index.php
@@ -88,11 +88,11 @@
             <input name="incNum" id="incNum" type="checkbox"/>
         </label>
 
+        <span>Case Options:</span><br/>
         <fieldset id="caseOpt">
-            <span>Case Options:</span><br/>
-                <label><input type="radio" name="caseOpt" value="allLower" checked><span>* Case (ALL)</span></label>
-                <label><input type="radio" name="caseOpt" value="allUpper"><span>* UPPER CASE (ALL)</span></label>
-                <label><input type="radio" name="caseOpt" value="firstUpper"><span>* Upper Case (All First Letters)</span></label>
+            <label><input type="radio" name="caseOpt" value="allLower" checked><span>* Case (ALL)</span></label>
+            <label><input type="radio" name="caseOpt" value="allUpper"><span>* UPPER CASE (ALL)</span></label>
+            <label><input type="radio" name="caseOpt" value="firstUpper"><span>* Upper Case (All First Letters)</span></label>
         </fieldset>
 
         <label for="capFirstChar"><span>Upper case first word?</span>
